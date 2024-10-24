007c6731939fa586cea7f792f1d88e50cbf3e5b9
Ken Ballon <bkenn003@gmail.com>
2024-10-24 10:38:38 +0800

    update

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ function enqueue_load_fa()
 
 function chr_theme_enqueue_styles()
 {
-	wp_register_style('custom-style', get_stylesheet_directory_uri() . '/css/style.css', [], '1.0.12', 'all');
+	wp_register_style('custom-style', get_stylesheet_directory_uri() . '/css/style.css', [], '1.0.13', 'all');
 	wp_enqueue_style('custom-style');
 }
 
